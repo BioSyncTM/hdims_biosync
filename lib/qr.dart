5331dc3ba7aec4c17233885ca9e11af66e23ddd1
CrimsonRuby11 <skprithviraj8@gmail.com>
2024-09-11 15:49:52 +0530

    Update qr.dart

--- lib/qr.dart
+++ lib/qr.dart
@@ -1,18 +1,6 @@
 
-import 'dart:convert' show jsonDecode, jsonEncode;
-import 'dart:io';
-import 'dart:async';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter_tesseract_ocr/flutter_tesseract_ocr.dart';
-import 'package:path/path.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:logging/logging.dart';
-import 'package:hdims_biosync/main.dart';
+
 class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
