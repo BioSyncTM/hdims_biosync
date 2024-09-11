33e074c7e891eb95904a6b5641320180caf9ac4b
CrimsonRuby11 <skprithviraj8@gmail.com>
2024-09-11 21:06:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:ffi';
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
 
 import 'package:flutter/material.dart';
 import 'package:hdims_biosync/qr.dart';
@@ -163,133 +164,135 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: Container(
-        // Top Bar
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // TopBar
-            Container(
-              margin: EdgeInsets.only(top: 20, left: 20),
-              child: const Text(
-                "Welcome, User!",
-                style: TextStyle(
-                  fontSize: 30
+      body: SingleChildScrollView(
+        child: Container(
+          // Top Bar
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // TopBar
+              Container(
+                margin: EdgeInsets.only(top: 20, left: 20),
+                child: const Text(
+                  "Welcome, User!",
+                  style: TextStyle(
+                    fontSize: 30
+                  ),
                 ),
               ),
-            ),
-
-            // Search bar
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Container(
-                    margin: EdgeInsets.all(20),
-                    child: TextField(
-                      onTapOutside: (event) {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.black,
+        
+              // Search bar
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Container(
+                      margin: EdgeInsets.all(20),
+                      child: TextField(
+                        onTapOutside: (event) {
+                          FocusManager.instance.primaryFocus?.unfocus();
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black,
+                            ),
+                            borderRadius: BorderRadius.circular(30),
                           ),
-                          borderRadius: BorderRadius.circular(30),
+                          labelText: "Search"
                         ),
-                        labelText: "Search"
                       ),
                     ),
                   ),
-                ),
-
-                // Quick Glance
-                Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        child: Container(
-                          margin: EdgeInsets.only(left: 20, right: 20),
-                          padding: EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(75, 0, 66, 74),
-                            border: Border.all(
-                              width: 1,
-                            ),
-                            borderRadius: BorderRadius.circular(30)
-                          ),
-                          child: Column(
-                            children: [
-                              Text(
-                                "Quick Glance",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                ),
-                              ),
-                        
-                              ConstrainedBox(
-                                constraints: BoxConstraints(maxHeight: 80),
-                                child: SingleChildScrollView(
-                                  child: Column(
-                                    children: [
-                                      Text(
-                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ]
-                ),
-
-                Container(
-                  margin: EdgeInsets.all(20),
-                  child: Row(
+        
+                  // Quick Glance
+                  Row(
                     children: [
                       Expanded(
                         child: Container(
-                          height: 200,
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(75, 0, 66, 74),
-                            borderRadius: BorderRadius.circular(30),
-                            border: Border.all(
-                              width: 1,
-                            )
-                          ),
                           child: Container(
-                            margin: EdgeInsets.all(20),
+                            margin: EdgeInsets.only(left: 20, right: 20),
+                            padding: EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(75, 0, 66, 74),
+                              border: Border.all(
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(30)
+                            ),
                             child: Column(
                               children: [
                                 Text(
-                                  "Notice",
+                                  "Quick Glance",
                                   style: TextStyle(
-                                    fontSize: 20
+                                    fontSize: 20,
                                   ),
                                 ),
-                                Text(
-                                  "No announcements."
+                          
+                                ConstrainedBox(
+                                  constraints: BoxConstraints(maxHeight: 80),
+                                  child: SingleChildScrollView(
+                                    child: Column(
+                                      children: [
+                                        Text(
+                                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                                        ),
+                                      ],
+                                    ),
+                                  ),
                                 )
                               ],
                             ),
                           ),
                         ),
-                      )
-                    ],
+                      ),
+                    ]
                   ),
-                )
-              ],
-            ),
-
-            // Quick Glance
-            
-          ],
+        
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 200,
+                            decoration: BoxDecoration(
+                              color: const Color.fromARGB(75, 0, 66, 74),
+                              borderRadius: BorderRadius.circular(30),
+                              border: Border.all(
+                                width: 1,
+                              )
+                            ),
+                            child: Container(
+                              margin: EdgeInsets.all(20),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Notice",
+                                    style: TextStyle(
+                                      fontSize: 20
+                                    ),
+                                  ),
+                                  Text(
+                                    "No announcements."
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+        
+              // Quick Glance
+              
+            ],
+          ),
+        
         ),
-
       ),
     );
   }
